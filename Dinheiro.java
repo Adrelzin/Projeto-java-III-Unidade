8f448ee88ac5eebe319daab40ae84e4c46df7331
Adryell William <adrelwilion@gmail.com>
2025-10-13 18:25:48 -0300

    Update Dinheiro.java

--- Dinheiro.java
+++ Dinheiro.java
@@ -1,12 +1,22 @@
-
 public class Dinheiro extends Pagamento {
     private double troco;
 
-    @Override
-    public void processarPagamento() {
-        
-        System.out.println("Processando pagamento em dinheiro. Troco: " + troco);
+    public Dinheiro(int idPagamento, double valor, double troco){
+        super(idPagamento, valor);
+        this.troco = troco;
     }
 
+    public double getTroco() {
+        return this.troco;
+    }
+
+    public void setTroco(double troco) {
+        this.troco = troco;
+    }
+
+    @Override
+    public void processarPagamento() {
+        System.out.println("Processando pagamento em dinheiro. Troco: " + troco);
+    }
     
 }
