2989904a6178878dd1c16fb2c1d8e217134f6f90
Adryell William <adrelwilion@gmail.com>
2025-10-13 18:26:35 -0300

    Update Pagamento.java

--- Pagamento.java
+++ Pagamento.java
@@ -1,6 +1,27 @@
 public abstract class Pagamento {
-    protected int idPagamento;
-    protected double valor;
+    private int idPagamento;
+    private double valor;
+	
+	public Pagamento(int idPagamento, double valor){
+		this.idPagamento = idPagamento;
+		this.valor = valor;
+	}
+
+	public int getIdPagamento(){
+		return this.idPagamento;
+	}
+	
+	public void setIdPagamento(int idPagamento){
+			this.idPagamento = idPagamento;
+	}
+	
+	public double getValor(){
+		return this.valor;
+	}
+	
+	public void setValor(double valor){
+			this.valor = valor;
+	}
 
     public abstract void processarPagamento();
     
