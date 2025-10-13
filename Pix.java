c99cf50683fb9f2a52534d6dba11ea99d86082f8
Adryell William <adrelwilion@gmail.com>
2025-10-13 18:27:05 -0300

    Update Pix.java

--- Pix.java
+++ Pix.java
@@ -1,12 +1,22 @@
-
 public class Pix extends Pagamento {
     private String chavePix;
 
-    @Override
-    public void processarPagamento() {
-        
-        System.out.println("Processando pagamento via Pix para chave: " + chavePix);
+    public Pix(int idPagamento, double valor, String chavePix){
+        super(idPagamento, valor);
+        this.chavePix = chavePix;
     }
 
+    public String getChavePix() {
+        return this.chavePix;
+    }
+
+    public void setChavePix(String chavePix) {
+        this.chavePix = chavePix;
+    }
+
+    @Override
+    public void processarPagamento() {
+        System.out.println("Processando pagamento via Pix para chave: " + chavePix);
+    }
     
 }
